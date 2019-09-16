a7fe981addc88fce0f726f0a98d2d417816f6b0c
marcuspereiradev <vinnycoast@gmail.com>
2019-09-16 16:48:48 -0300

    testing The Controller  getAll() method

--- controllers/books.js
+++ controllers/books.js
@@ -1,7 +1,21 @@
+const defaultResponse = (data, statusCode = 200) => ({
+  data,
+  statusCode
+});
+const errorResponse = (message, statusCode = 400) => defaultResponse({
+  error: message
+}, statusCode);
+
 class BooksController {
   constructor(Books) {
     this.Books = Books;
   }
 
-  getAll() {}
+  getAll() {
+    return this.Books.findAll({})
+      .then((result) => defaultResponse(result))
+      .catch(error => errorResponse(error.message));
+  }
 }
+
+export default BooksController;
--- test/unit/controllers/books.js
+++ test/unit/controllers/books.js
@@ -1,7 +1,24 @@
+import BooksController from '../../../controllers/books';
+
 describe('Controllers: Book', () => {
   describe('Get all books: getAll()', () => {
     it('should return a list of books', () => {
-      
+      const Books = {
+        findAll: td.function()
+      };
+
+      const expectedResponse = [{
+        id: 1,
+        name: 'Test Book',
+        created_at:'2019-09-16T15:05:36.6922',
+        updated_at:'2019-09-16T15:05:36.6922',
+      }];
+
+      td.when(Books.findAll({})).thenResolve(expectedResponse)
+
+      const booksController = new BooksController(Books);
+      return booksController.getAll()
+        .then(response => expect(response.data).to.be.eql(expectedResponse));
     });
   });
 });
